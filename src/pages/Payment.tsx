8a44daadc7d436479925d841a3df0f03582c94ca
Meet-Thakkar21 <meetthakkar3333@gmail.com>
2026-04-23 15:47:48 +0530

    FIx build errors

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { QrCode, CheckCircle2, AlertCircle } from 'lucide-react';
 import { Button } from '../components/Button';
